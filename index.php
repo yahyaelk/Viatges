8058fbbaff188db9e265425e177ec208cc70961d
Yahya <a17yahelekaj@inspedralbes.cat>
2019-12-09 11:58:46 +0100

    div bienvenida

--- index.php
+++ index.php
@@ -7,21 +7,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
     <title>Viatges</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-      <div class="container">
-      <img class="navbar-brand js-scroll-trigger"src="">
-      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Inicia Sessió
-      </button>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Registrarse
-      </button>
-      </div>
-  </nav>
+
+<div id="bienvenida" class="d-flex justify-content-center">
+    <div class="col-8">
+        <div class="row">
+            <div class="col-5 col-xs-12">
+                <img src="img/main-avion.gif" height="280" width="437.5">
+            </div>
+            <div class="col-7 col-xs-12">
+                <h1>Benvingut</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae officia cumque nostrum neque, incidunt corrupti. Eaque praesentium modi cumque amet aliquam ea fugit explicabo dolores quod sapiente? Repudiandae, quae. Ut.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 
 </html>
