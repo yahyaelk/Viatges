411ed0560d441100f565c3f665cd6f6f51996da6
You <you@gmail.com>
2019-12-09 11:53:32 +0100

    logo y opciones de registro y login

--- index.php
+++ index.php
@@ -5,23 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/css.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     
     <title>Viatges</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-      <div class="container">
-      <img class="navbar-brand js-scroll-trigger"src="">
-      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Inicia Sessió
-      </button>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Registrarse
-      </button>
-      </div>
-  </nav>
+  <div class="flexible">
+    <div>
+      <img id="logo" src="logo_rombo/facebook_cover_photo_1.png">
+    </div>
+    <div>
+      <button type="button" class="btn btn-light">Inicia Sessió</button>
+      <button type="button" class="btn btn-light">Registrarse</button>
+    </div>
+  </div>
 </body>
 
 </html>
