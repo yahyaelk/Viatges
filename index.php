23bb671268b5d3c7e9329caf95360f7976217782
You <you@gmail.com>
2019-12-09 10:40:52 +0100

    inicia

--- index.php
+++ index.php
@@ -7,9 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Viatges</title>
 </head>
-
 <body>
-
+<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+      <div class="container">
+      <img class="navbar-brand js-scroll-trigger"src="">
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Inicia Sessió
+      </button>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Registrarse
+      </button>
+      </div>
+  </nav>
 </body>
 
 </html>
