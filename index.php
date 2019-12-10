d6ca915189f4491f7de81e44ae1b0f606b9f4514
a18sonvargar <a18sonvargar@inspedralbes.cat>
2019-12-10 09:08:11 +0100

    canvis index.php i afegit arxius .js

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
     <?php
     require_once('model/experiencia.php');
     ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Viatges</title>
 </head>
@@ -24,7 +31,36 @@
             </div>
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
-                <button type="button" class="btn btn-light">Inicia Sessió</button>
+                <div class="container">
+                    <!-- Trigger the modal with a button -->
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Inicia Sessió</button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog">
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h1 class="modal-title h3 mb-3 font-weight-normal">Please sign in</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form class="form-signin">
+                                        <label for="inputEmail" class="sr-only">Email address</label>
+                                        <input type="email" id="inputEmail" class="form-control"
+                                            placeholder="Email address" required autofocus>
+                                        <label for="inputPassword" class="sr-only">Password</label>
+                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
                 <button type="button" class="btn btn-light">Registrarse</button>
             </div>
         </div>
