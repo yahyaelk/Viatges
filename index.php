d1973c01b899901d9c2e5cfbad6ae4d886557cf0
a18sonvargar <a18sonvargar@inspedralbes.cat>
2019-12-11 10:15:40 +0100

    canvis en index.php i en el favicon

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
-    <link rel="icon" href="logos/favicon.png">
+    <link rel="icon" href="logos/favicon_3.png">
     
     <title>Viatges</title>
 </head>
@@ -58,6 +58,7 @@
                                 <label for="inputPassword" class="sr-only">Password</label>
                                 <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
                             </form>
+                            <p id= "message"></p>
                         </div>
                         <div class="modal-footer">
                             <button type= "button" id= "botoIni" class="btn btn-lg btn-primary btn-block">Sign in</button>
